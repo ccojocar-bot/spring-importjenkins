f2b80ec545973c12293eee2f28b212bae458396f
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2019-11-22 12:19:55 +0100

    Initial import

---
+++ src/test/java/com/example/springimportjenkins/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springimportjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
